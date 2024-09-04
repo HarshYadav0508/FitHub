3357830640e190ef8234d8b41c1fc9c9af6fde8e
HarshYadav0508 <heyitsharshyadav@gmail.com>
2024-09-05 04:45:47 +0530

    setting up enrolled classes

--- frontend/src/pages/Dashboard/Student/Enroll/EnrolledClasses.jsx
+++ frontend/src/pages/Dashboard/Student/Enroll/EnrolledClasses.jsx
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from 'react'
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import useUser from '../../../../hooks/useUser';
 
 const EnrolledClasses = () => {
   const [data, setData] = useState([]);
+  const axioSecure = useAxiosSecure();
+  const {currentUser} = useUser();
 
-
-  useEffect
+  useEffect (() => {
+    axioSecure.get(`/enrolled-classes/${currentUser?.email}`)
+    .then(res => {
+      setData(res.data);
+    }).catch(err => console.log(err))
+  }, [])
 
   return (
-    <div>EnrolledClasses</div>
+    <div>
+      <h1 className='text-2xl my-6`'>Enrolled Classes</h1>
+      <div>
+        {
+          data.map((item, index) => (
+            <div></div>
+              
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
